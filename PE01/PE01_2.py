e965cf3a6d53cfd429617038e2ff217d1b95bf2d
deevashree <deevashree12345@gmail.com>
2025-02-04 19:36:46 +0000

    PEO1

---
+++ PE01/PE01_2.py
@@ -0,0 +1,10 @@
+print("Item\t\tprice  ")
+print("Apple\t\t$1.75\nbanana\t\t$2.25\nCherry\t\t$3.50")
+print("-------\t\t-------")
+print("Total\t\t$7.50")
+
+#print("2+3*4")
+
+f=3
+g=2
+print(f*g)
---
+++ PE01/PEO1_4.py
@@ -0,0 +1,5 @@
+price=99.99
+discountpercent=25
+markdown=(discountpercent/100*price)
+print("price =",round(price-markdown,2))
+#print(round(74.992,249))
