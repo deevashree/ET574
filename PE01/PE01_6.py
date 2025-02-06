2880ab8c256ff7757e48cb8bba7975c87ad10e38
deevashree <deevashree12345@gmail.com>
2025-02-06 19:38:16 +0000

    hgt5brtb4

---
+++ PE01/PE01_6.py
@@ -0,0 +1,39 @@
+print(2+3*4)
+print(1-7**2)
+print(1//2**3)
+print((3+4)*5)
+print((5%3)*4)
+print(7//3)
+print(14%4)
+print(1+7%4)
+print(14//4*4)
+print(5//2+2)
+print(5%5*5)
+
+NewYear.sales #invalid the period is a sepecial character.
+room&color #invalid & is a special character
+TOrF_1040 # invalid _ is a special character
+311HotLine # invalid this starts with a number
+expense#  # invalid # is a special character
+INCOME 101 # valiid
+
+cost= 8
+sum= 5
+rate= 7
+total=9
+product=20
+quotient= 16
+num= 2
+
+print(cost+5)
+print(sum*rate)
+print(product/10)
+print(cost//num)
+print(total-cost)
+print(quotient % rate)
+
+a=5
+b=3
+int(-a/b)
+round(a/b,2)
+abs(b-a)
